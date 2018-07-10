dcc70872481239bc0b2267c50f77d1f3c608753d
Danielthweatt <dthweatt192@yahoo.com>
2018-07-10 16:03:52 -0500

    preparing for deployment on heroku

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 // Connection to MongoDB
+mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nytreact');
 
 // API server Start
